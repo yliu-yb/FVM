996eaaff776502d2ecbbe5fe9ab7c5d589826758
yliu1yb <yliu.yb@gamil.com>
2022-08-21 01:57:44 +0800

    added hybrid diff scheme

--- src/main.f90
+++ src/main.f90
@@ -8,40 +8,54 @@ program SCMExtension
     use customTypes
     use CentralDifferencingCalc1DimConvectionDiffusion
     use UpwindDifferencingCalc1DimConvectionDiffusion
+    use HybridDifferencingCalc1DimConvectionDiffusion
     implicit none
 
     type(FVM_1DimType) :: para
     integer :: mk
 
-    print *, "central differencing"
-    print *, "case1"
-    para = FVM_1DimType(1.0, 0.0, 1.0, 0.1, 1.0, 0.1, 5)
+    print *, "central differencing scheme"
+    ! print *, "case1"
+    ! para = FVM_1DimType(1.0, 0.0, 1.0, 0.1, 1.0, 0.1, 5)
+    ! mk = CentralDiffCalc1DimConvectionDiffusion(para)
+
+    ! print *, "case2"
+    para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 25)
     mk = CentralDiffCalc1DimConvectionDiffusion(para)
 
-    print *, "case2"
+    ! print *, "case3"
+    ! para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 20)
+    ! mk = CentralDiffCalc1DimConvectionDiffusion(para)
+
+    ! print *, "upwind differencing scheme"
+    ! print *, "case1 - positive u"
+    ! para = FVM_1DimType(1.0, 0.0, 1.0, 0.1, 1.0, 0.1, 5)
+    ! mk = UpwindDiffCalc1DimConvectionDiffusion(para)
+
+    ! print *, "case1 - negtive u"
+    ! para = FVM_1DimType(0.0, 1.0, 1.0, -0.1, 1.0, 0.1, 5)
+    ! mk = UpwindDiffCalc1DimConvectionDiffusion(para)
+
+    ! print *, "case2 - positive u"
+    ! para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 5)
+    ! mk = UpwindDiffCalc1DimConvectionDiffusion(para)
+
+    ! print *, "case2 - negtive u"
+    ! para = FVM_1DimType(0.0, 1.0, 1.0, -2.5, 1.0, 0.1, 5)
+    ! mk = UpwindDiffCalc1DimConvectionDiffusion(para)
+
+    print *, "hybrid differencing scheme"
+    print *, "case2 - positive u, node num: 5"
     para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 5)
-    mk = CentralDiffCalc1DimConvectionDiffusion(para)
+    mk = HybirdDiffCalc1DimConvectionDiffusion(para)
 
-    print *, "case3"
-    para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 20)
-    mk = CentralDiffCalc1DimConvectionDiffusion(para)
-
-    print *, "upwind differencing"
-    print *, "case1 - positive u"
-    para = FVM_1DimType(1.0, 0.0, 1.0, 0.1, 1.0, 0.1, 5)
-    mk = UpwindDiffCalc1DimConvectionDiffusion(para)
-
-    print *, "case1 - negtive u"
-    para = FVM_1DimType(0.0, 1.0, 1.0, -0.1, 1.0, 0.1, 5)
-    mk = UpwindDiffCalc1DimConvectionDiffusion(para)
-
-    print *, "case2 - positive u"
-    para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 5)
-    mk = UpwindDiffCalc1DimConvectionDiffusion(para)
-
-    print *, "case2 - negtive u"
+    print *, "case2 - negtive u, node num: 5"
     para = FVM_1DimType(0.0, 1.0, 1.0, -2.5, 1.0, 0.1, 5)
-    mk = UpwindDiffCalc1DimConvectionDiffusion(para)
+    mk = HybirdDiffCalc1DimConvectionDiffusion(para)
+
+    print *, "case2 - positive u, node num: 25"
+    para = FVM_1DimType(1.0, 0.0, 1.0, 2.5, 1.0, 0.1, 25)
+    mk = HybirdDiffCalc1DimConvectionDiffusion(para)
 
 end program SCMExtension
     
--- src/test.f90
+++ src/test.f90
@@ -2,7 +2,7 @@ program name
     implicit none
     real :: F
     F = -4.5
-    print *, max(F, 0.0)
-    print *, max(-F, 0.0)
+    print *, max(F, 0.0, 5.0)
+    print *, max(-F, 0.0, -5.0)
 
 end program name
